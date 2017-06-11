fbce6239f8d0cf3ce8753ad4fceea32a4d3fe5d8
陈冲 <chenchong8806@gmail.com>
2017-06-11 18:54:48 +0800

    局部属性委托

--- src/main/kotlin/com/foolchen/kotlin/java/object/DelegateMapExample.kt
+++ src/main/kotlin/com/foolchen/kotlin/java/object/DelegateMapExample.kt
@@ -32,8 +32,20 @@ fun main(args: Array<String>) {
     mutableUser.map.put("other", false)
 
     // 通过定义的委托属性来从Map中取值
-    println(user.name)
-    println(user.age)
+    println(mutableUser.name)
+    println(mutableUser.age)
     // 通过map可以打印出所有的值，但是在未定义委托属性的情况下无法通过属性从Map中取值（如此处的other属性）
-    println(user.map.toString())
+    println(mutableUser.map.toString())
+
+
+    // 局部属性委托
+    example { mutableUser }
+    println(mutableUser.map.toString()) // 第一次调用时，会执行Lazy Name的赋值操作
+    println(mutableUser.map.toString()) // 再次调用时，则不会执行Lazy Name的赋值操作
+}
+
+fun example(computeUser: () -> MutableMappedUser) {
+    val memoizedUser by lazy(computeUser)
+    memoizedUser.name = "Lazy name"
+    println("Lazy Init")
 }
