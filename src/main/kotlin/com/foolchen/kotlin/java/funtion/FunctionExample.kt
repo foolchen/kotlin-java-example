ae5d454388668c6c0ae0feb102fc990c20a61137
陈冲 <chenchong8806@gmail.com>
2017-06-14 20:48:38 +0800

    增加了部分函数定义的演示

---
+++ src/main/kotlin/com/foolchen/kotlin/java/funtion/FunctionExample.kt
@@ -0,0 +1,32 @@
+package com.foolchen.kotlin.java.funtion
+
+/**
+ * @author chenchong. Created on 2017/6/14.
+ */
+
+// 为Int类型定义扩展
+// 定义了整数的除运算
+infix fun Int.divider(x: Int): Int {
+    return this / x
+}
+
+// 定义了一个普通的函数
+// 其中s必须传入且不为空，off和len有默认值
+fun read(s: String, off: Int = 0, len: Int = s.length) {
+    println("off=$off,len=$len")
+}
+
+fun main(args: Array<String>) {
+    // 针对一个值进行除法计算
+    val number = 9
+    println(number divider 2)
+    println(number.divider(2))
+
+    // 在其他参数有默认值的情况下，可以只传入不为空的参数即可
+    read("123")
+    // 可以选择部分参数传入，减少了重载方法
+    read("456", 10)
+    // 为了避免歧义，最好使用指定参数名的方式传入部分参数
+    read("789", off = 11)
+    read("789", len = 20)
+}
